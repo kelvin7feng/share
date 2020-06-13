e8bd99c0202a82170d91f345856552787535dd1a
kelvin7feng <kelvin7.feng@gmail.com>
2020-06-13 22:43:53 +0800

    属性修改

--- cfg_mgr/pkgEquipCfgMgr.lua
+++ cfg_mgr/pkgEquipCfgMgr.lua
@@ -1,20 +1,5 @@
 doNameSpace("pkgEquipCfgMgr")
 
-AttrLanguage = {
-    attr1           = 1001,
-    attr2           = 1002,
-    attr3           = 1003,
-    attr4           = 1004,
-    attr5           = 1005,
-    attr6           = 1006,
-    attr7           = 1007,
-    attr8           = 1008,
-    attr9           = 1009,
-    attr10          = 1010,
-    attr11          = 1011,
-    attr12          = 1012,
-}
-
 m_tbLevelUpCfg = m_tbLevelUpCfg or {}
 
 function Init()
@@ -49,10 +34,10 @@ function GetAttrDescList(dId)
     local dAttr = nil
     local dUnit = 0
     local dLId = 0
-    for i=1,11 do
+    for i=1,12 do
         dAttr = tbCfg["attr"..i]
         dUnit = tbCfg["unit"..i]
-        dLId = AttrLanguage["attr"..i]
+        dLId = pkgAttrMgr.AttrLanguage["attr"..i]
         if dAttr > 0 then
             if dUnit == 1 then
                 dAttr = dAttr
@@ -80,7 +65,7 @@ function GetLevelUpAttrDescList(dEquipId, dLevel)
     for i=1,11 do
         dAttr = tbCfg["attr"..i]
         dUnit = tbCfg["unit"..i]
-        dLId = AttrLanguage["attr"..i]
+        dLId = pkgAttrMgr.AttrLanguage["attr"..i]
         if dAttr > 0 then
             if dUnit == 1 then
                 dAttr = dAttr
