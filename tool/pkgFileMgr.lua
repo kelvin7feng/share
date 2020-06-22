aeff7095012d75441760001b1bb1bc44e8a4b8b0
kelvin7feng <kelvin7.feng@gmail.com>
2020-06-22 23:40:22 +0800

    怪物

--- tool/pkgFileMgr.lua
+++ tool/pkgFileMgr.lua
@@ -21,3 +21,37 @@ function readContentFromFile(strFilePath)
 
 	return str
 end
+
+function FileExists(strFilePath)
+    return KG.CustomFile.IsFileExits(strFilePath)
+end
+
+local function GetPersistentPath(strFileName)
+	local filePath = string.format("%s%s", __persistentDataPath, strFileName)
+	if not FileExists(filePath) then
+		return
+	end
+
+	return filePath
+end
+
+local function GetStreamingPath(strFileName)
+	local filePath = string.format("%s%s", __streamingAssetsPath, strFileName)
+	if UnityEngine.Application.platform == UnityEngine.RuntimePlatform.Android then
+		if not KG.CustomFile.IsAndroidAssetExist(strFileName) then
+			return
+		end
+	elseif not FileExists(filePath) then
+		return
+	end
+	return filePath
+end
+
+function GetAssetPath(strFileName)
+    local strFilePath = GetPersistentPath(strFileName)
+	if not strFilePath then
+		strFilePath = GetStreamingPath(strFileName)
+	end
+
+    return strFilePath
+end
